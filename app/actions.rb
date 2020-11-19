e45ba9c9df0661d590dfa956bc0ae6929ffdf0fd
mariehfaucher <mariehfaucher@hotmail.com>
2020-11-19 04:37:17 +0000

    method in action

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,15 @@
 get '/' do
-  File.read(File.join('app/views', 'index.html'))
+  finstagram_post = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
+    time_ago_in_minutes: 15,
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+    username: "sharky_j",
+    text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+}]
+
+  humanized_time_ago(finstagram_post[:finstagram_post])
 end
